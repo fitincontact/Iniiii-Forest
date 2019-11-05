3e9316503edc6f0ac8620c0216101fa159a5f5aa
Vsevolod Fitin <fitincontact@gmail.com>
2019-11-05 11:53:31 +0300

    2

--- work/main.lua
+++ work/main.lua
@@ -1,5 +1,18 @@
-function love.draw()
+--function love.draw()
     --love.graphics.print("Hello World", 40, 30)
 	--love.graphics.circle("fill", 10, 10, 100, 25)
-	love.graphics.rectangle("line", 1, 1, 312, 239)
-end
+	--love.graphics.rectangle("line", 1, 1, 318, 238)
+	--end
+	
+	function love.load()
+		x = 100
+	end
+
+	function love.update()
+		print(dt)
+		x = x + 5
+	end
+
+	function love.draw()
+		love.graphics.rectangle("line", x, 50, 200, 150)
+	end
