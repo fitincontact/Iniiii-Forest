6499d092dee996a8a8d4cfa54e122539649f4ab8
Vsevolod Fitin <fitincontact@gmail.com>
2019-11-07 21:57:06 +0300

    Libraries

--- work/main.lua
+++ work/main.lua
@@ -1,7 +1,21 @@
---! file: main.lua
---Leave out the .lua
--- No need for love.load or whatever
-require("example")
-require("e.r")
---print(test)
---print(test)
+function love.load()
+	tick = require "tick"
+
+	--Create a boolean
+	drawRectangle = false
+
+	--The first argument is a function
+	--The second argument is the time it takes to call the function
+	tick.recur(function () drawRectangle = true end ,   2)
+end
+
+function love.update(dt)
+	tick.update(dt)
+end
+
+function love.draw()
+	--if drawRectangle is true then draw a rectangle
+	if drawRectangle then
+		love.graphics.rectangle("fill", 100, 100, 300, 200)
+	end
+end
