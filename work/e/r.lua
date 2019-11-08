9cfe40bf773461cce0ff275c94f463ca1502ff9d
Vsevolod Fitin <fitincontact@gmail.com>
2019-11-09 01:02:03 +0300

    Images

--- work/e/r.lua
+++
@@ -1,9 +0,0 @@
---
--- Created by IntelliJ IDEA.
--- User: vsevolodfitin
--- Date: 07.11.2019
--- Time: 20:21
--- To change this template use File | Settings | File Templates.
---
-test2 = 30
-
--- work/main.lua
+++ work/main.lua
@@ -1,24 +1,26 @@
---! file: main.lua
 function love.load()
-    Object = require "library.classic"
-    require "object.shape"
-    require "object.rectangle"
-    require "object.circle"
-
-    r1 = Rectangle(100, 100, 200, 50, 100)
-    r2 = Rectangle(0, 0, 25, 140, 33)
-    c1 = Circle(10, 200, 18)
+    myImage = love.graphics.newImage("image/sprite/dynamic/hero_01_00.png")
+    width = myImage:getWidth()
+    height = myImage:getHeight()
+    love.graphics.setBackgroundColor(1, 1, 1)
 end
+--[[function love.draw()
 
-function love.update(dt)
-    r1:update(dt)
-    r2:update(dt)
-    c1:update(dt)
-end
+    love.graphics.draw(myImage, 100, 100)
+    love.graphics.draw(myImage, 100, 100, 10)
+    love.graphics.draw(myImage, 100, 100, 0, 2, 2)
+    love.graphics.draw(myImage, 100, 100, 0, -1, 1)
+    love.graphics.draw(myImage, 100, 100, 0, 2, 2, 39, 50)
+    --love.graphics.draw(myImage, 100, 100, 0, 2, 2, 39, 50, 15, 20)
+    love.graphics.draw(myImage, 100, 100, 0, 2, 2, width/2, height/2)
+end]]
 
 function love.draw()
-    r1:draw()
-    r2:draw()
-    c1:draw()
+    love.graphics.setColor(255/255, 200/255, 40/255, 127/255)
+    love.graphics.setColor(1, 0.78, 0.15, 0.5)
+    -- Or ...
+    love.graphics.draw(myImage, 100, 100)
+    -- Not passing an argument for alpha automatically sets it to 1 again.
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.draw(myImage, 200, 100)
 end
-
